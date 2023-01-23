f246ea90e0c204e026c58338ef2145b150e482d6
Benjamin <bcott93@gmail.com>
2023-01-23 23:01:11 +0000

    Debugged + integrated HTML

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,7 +1,7 @@
 // Quiz questions, set up as objects within an array. Allowing for easy access. 
 const questions = [
     {
-        question: "What code is most likely used to allow you to interact with this quiz?",
+        question: "What code has most likely been used to allow you to interact with this quiz?",
         answers: {
             a: "HTML",
             b: "JavaScript",
@@ -25,7 +25,7 @@ const questions = [
             a: "let i = 0; i < arr.length; i++",
             b: "const i = 0; i++, i > arr.length; i++",
             c: "var i = 0; i > arr.length; i++"
-        }
+        },
         correctAnswer: "a"
     },
     {   
@@ -34,7 +34,7 @@ const questions = [
             a: "Always showing up for work on time",
             b: "A variable that can be amended as you continue through the document",
             c: "A variable which is unchangeable"
-        }
+        },
         correctAnswer: "c"
     },
     {
@@ -47,54 +47,58 @@ const questions = [
         correctAnswer: "b"
     }]
 
-// Selects time 
-let time = document.querySelector("#time")
-// Selects start
-let start = document.querySelector("#start")
-// Sets the start of the timer
-let secondsRemaining = 60
-// Shows the remaining time
-time.textContent = secondsRemaining
-// Setting question to 0 for use in the a loop later on
-let currentQuestion = 0 
-// Setting score
-let score = 0
+
+let time = document.querySelector("#time") // Timer display 
+let start = document.querySelector("#start") // Start Button
+let secondsRemaining = 60 // Sets the start of the timer
+time.textContent = secondsRemaining // Shows the remaining time
+let currentQuestion = 0  // Setting question to 0 for use in the a loop later on
+let score = 0 // Setting score
+let startScreen = document.getElementById("start-screen")
+let questionDiv = document.getElementById("questions"); // Question Div
+let questionTitle = document.getElementById("question-title"); // Set the questions to the title
+let clearPrev = document.getElementById("choices") // Removes previous choices from the quiz
 
 // start quiz
 document.getElementById("start").addEventListener("click", startQuiz)
 
 function startQuiz() {
-    document.getElementById("start").style.display = "none";
-    showQuestion();
-    startTimer();
+    showQuestion()
+    startTimer()
+    displayQuestions()
 }
+
+
+function displayQuestions() {
+    startScreen.classList.add("hide")
+    questionDiv.classList.remove("hide")
+}
+
 
 // Goes into the questions array and navigates to the current question and finds the data for the loop 
 function showQuestion() {
     let question = questions[currentQuestion].question
     let answer = questions[currentQuestion].answers
-// check what this does
-    document.getElementById("question").innerHTML = question
-}
+    questionTitle.textContent = question
+    clearPrev.innerHTML = ""
+    
+    // Enables the answer buttons on the DOM
+    let choices = document.getElementById("choices")
 
-
-
-
-
-// Enables the answer buttons on the DOM
-let choices = document.getElementById("choices")
-
-for (let letter in answers) {
+    for (let letter in answer) {
     // Creating the buttons
-    let button = document.createElement("button")
+    let button = document.createElement("answer-button")
     // Adding CSS to the JS
-    button.classList.add("button")
+    button.classList.add("answer-button")
     // Labeling each button
     button.value = letter
-    // Giving the 
-    button.innerHTML = letter + ": " + answers[letter]
+    // Giving the buttons text
+    button.innerHTML = letter + ": " + answer[letter]
     // adding the buttons to the choices tag in HTML
     choices.appendChild(button)
+    // Triggers the check answer function when clicked
+    button.addEventListener("click", checkAnswer)
+    }
 }
 
 function checkAnswer(event) {
@@ -104,26 +108,26 @@ function checkAnswer(event) {
     if (userAnswer === correctAnswer) {
         score ++
     } else {
-        timer = timer - 10
+        secondsRemaining = secondsRemaining - 10
     }
 
-    currentQuestion++
-
-    if (currentQuestion === questions.length) {
+    if (currentQuestion === questions.length - 1) {
         endQuiz()
     } else {
+        currentQuestion++
         showQuestion()
     }
 }
 
 // ADD A COMMENT
-function starttimer() {
+function startTimer() {
     setInterval(function() {
-       time--
+       secondsRemaining--
        time.textContent = secondsRemaining
 
-       if(time === 0) {
-        endQuiz();
+       if(secondsRemaining === 0) {
+        clearInterval(time)
+        endQuiz()
        }
       }, 1000)
 
@@ -131,7 +135,7 @@ function starttimer() {
 
 // endQuiz function removes actions from an element
 function endQuiz() {
-    clear
+    clearInterval(time)
 }
 
 
